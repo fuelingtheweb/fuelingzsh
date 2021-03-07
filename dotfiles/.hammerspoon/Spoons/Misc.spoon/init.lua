98e6f5e5be6c7f911245c718bf0d3ccd49073e47
Nathan Morgan <nathan@fuelingtheweb.com>
2021-03-07 06:46:56 -0500

    Updates

--- dotfiles/.hammerspoon/Spoons/Misc.spoon/init.lua
+++ dotfiles/.hammerspoon/Spoons/Misc.spoon/init.lua
@@ -17,4 +17,20 @@ hs.urlevent.bind('misc-optionPressedTwice', function()
     end
 end)
 
+spoon.MouseCircle = hs.loadSpoon('vendor/MouseCircle')
+spoon.MouseCircle.color = {hex = '#367f71'}
+spoon.MouseCircle:bindHotkeys({show = {{'ctrl', 'alt', 'cmd'}, 'M'}})
+
+spoon.ReloadConfiguration = hs.loadSpoon('vendor/ReloadConfiguration')
+spoon.ReloadConfiguration.watch_paths = {hs.configdir, hs.configdir .. '/Spoons/Custom.spoon'}
+spoon.ReloadConfiguration:start()
+hs.notify.new({title = 'Hammerspoon', informativeText = 'Config loaded'}):send()
+
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'B', function()
+    app = hs.application.frontmostApplication()
+    bundle = app:bundleID()
+    hs.pasteboard.setContents(bundle)
+    hs.notify.new({title = 'App Bundle Copied', informativeText = bundle}):send()
+end)
+
 return obj
--- dotfiles/.hammerspoon/init.lua
+++ dotfiles/.hammerspoon/init.lua
@@ -1,28 +1,13 @@
 hs.loadSpoon('Variables')
 hs.loadSpoon('Functions')
+hs.loadSpoon('Watchers')
+
 Ray = hs.loadSpoon('Ray')
 Pending = hs.loadSpoon('Pending')
 Modal = hs.loadSpoon('Modal')
-hs.loadSpoon('Watchers')
 TextManipulation = hs.loadSpoon('TextManipulation')
 Artisan = hs.loadSpoon('Artisan')
+
 hs.loadSpoon('Misc')
 hs.loadSpoon('KarabinerHandler')
-
 hs.loadSpoon('Custom')
-
-spoon.MouseCircle = hs.loadSpoon('vendor/MouseCircle')
-spoon.MouseCircle.color = {hex = '#367f71'}
-spoon.MouseCircle:bindHotkeys({show = {{'ctrl', 'alt', 'cmd'}, 'M'}})
-
-spoon.ReloadConfiguration = hs.loadSpoon('vendor/ReloadConfiguration')
-spoon.ReloadConfiguration.watch_paths = {hs.configdir, hs.configdir .. '/Spoons/Custom.spoon'}
-spoon.ReloadConfiguration:start()
-hs.notify.new({title = 'Hammerspoon', informativeText = 'Config loaded'}):send()
-
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'B', function()
-    app = hs.application.frontmostApplication()
-    bundle = app:bundleID()
-    hs.pasteboard.setContents(bundle)
-    hs.notify.new({title = 'App Bundle Copied', informativeText = bundle}):send()
-end)
